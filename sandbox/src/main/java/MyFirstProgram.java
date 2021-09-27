c3b2a92bdf7abfd8a815dd27a46884d83ff2c8ca
olgamit <vollga91@gmail.com>
2021-09-27 16:29:11 +0700

    Метод вычисления расстояния между точками

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -5,14 +5,11 @@ public class MyFirstProgram {
 		Point A = new Point (0.5, 4);
 		Point B = new Point (8.5, 3);
 
-		double d = distance(A, B);
+		double d = A.distance(A, B);
 		System.out.println("Расстояние между точками составляет " + d);
 		}
 
-	public static double distance(Point p1, Point p2){
 
-		return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + ((p2.y-p1.y)*(p2.y-p1.y)));
-	}
 
 
 }
