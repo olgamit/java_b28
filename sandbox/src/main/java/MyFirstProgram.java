12553e75ac3de254951f6fd8f7457d348356d011
olgamit <vollga91@gmail.com>
2021-09-27 16:27:58 +0700

    Функция вычисления расстояния между точками

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,18 @@
 public class MyFirstProgram {
 
 	public static void main (String [] args) {
-		System.out.println("Hello, world!");
+
+		Point A = new Point (0.5, 4);
+		Point B = new Point (8.5, 3);
+
+		double d = distance(A, B);
+		System.out.println("Расстояние между точками составляет " + d);
 		}
+
+	public static double distance(Point p1, Point p2){
+
+		return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + ((p2.y-p1.y)*(p2.y-p1.y)));
+	}
+
+
 }
