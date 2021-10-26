d918dad168f5012aff1176599e04ef0843bcfb86
olgamit <vollga91@gmail.com>
2021-10-26 12:17:34 +0700

    Добавлен тест для модификации группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/b28/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.b28.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.b28.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
